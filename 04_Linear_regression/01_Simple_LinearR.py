6ec43a72e6b2455b3b0d1783859ac114c35d6f7c
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2026-01-31 12:28:30 +0530

    Standardization of independent var

--- 04_Linear_regression/01_Simple_LinearR.py
+++ 04_Linear_regression/01_Simple_LinearR.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('height_weight_linear_data.csv')
 
@@ -8,3 +9,9 @@ y = df['Height']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.24, random_state=51)
 
+#Standardization 
+
+scalar = StandardScaler()
+
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.transform(X_test) # Do not fit to prevent data leakage
