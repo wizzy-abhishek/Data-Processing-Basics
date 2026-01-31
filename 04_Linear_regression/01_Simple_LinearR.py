df9af36356896255e3a47cd4b13ae54fc70862fc
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2026-01-31 12:45:02 +0530

    Checkng performance metrics

--- 04_Linear_regression/01_Simple_LinearR.py
+++ 04_Linear_regression/01_Simple_LinearR.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
 df = pd.read_csv('height_weight_linear_data.csv')
 
@@ -24,3 +26,18 @@ regression = LinearRegression(n_jobs=-1)
 regression.fit(X_train, y_train)
 print(regression.coef_)
 print(regression.intercept_)
+
+#Prediction of Test data
+
+y_pred = regression.predict(X_test)
+
+#Checkng performance
+mse = mean_squared_error(y_test, y_pred)
+print(mse)
+
+mae = mean_absolute_error(y_test, y_pred)
+print(mae)
+
+print(np.sqrt(mse))
+
+print(regression.predict(scalar.transform([[84]])))
